2f271b32094b9b1329394dc65e99a4b3fdba10a0
EvansRobbie <evansrobbie5311@gmail.com>
2024-12-17 00:16:33 +0300

    tailwind before color

--- src/components/Greetings.tsx
+++ src/components/Greetings.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { motion } from 'framer-motion';
 
 export default function Greeting({ messages }: { messages: string[] }) {
   const randomMessage = () =>
@@ -7,9 +8,24 @@ export default function Greeting({ messages }: { messages: string[] }) {
   const [greeting, setGreeting] = useState(messages[0]);
 
   return (
-    <div>
-      <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>New Greeting</button>
-    </div>
+    <motion.div
+      initial={{ opacity: 0, filter: 'blur(5px)', y: 50 }}
+      animate={{ opacity: 1, filter: 'blur(0px)', y: 0 }}
+      className='bg-slate-100 p-4  rounded'
+    >
+      <h3>
+        {' '}
+        <span className='before:content before:absolute before:bg-red-500 relative before:block inline-block before:-inset-0 before:-skew-y-3 after:content-[" "] after:pl-1'>
+          <span className='relative text-white'>{greeting}!</span>
+        </span>{' '}
+        Thank you for visiting!
+      </h3>
+      <button
+        className='mt-4 bg-slate-500 text-white text-small px-6 py-2 rounded-lg text-sm'
+        onClick={() => setGreeting(randomMessage())}
+      >
+        New Greeting
+      </button>
+    </motion.div>
   );
 }
